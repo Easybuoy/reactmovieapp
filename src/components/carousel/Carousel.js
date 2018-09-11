c65d2cb4778045a42671d703c3536f73cfae36a3
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-11 15:19:18 +0100

    [feature]  done fixing carousel call using tmdb api

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import  {Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Container, Button } from 'mdbreact';
 import TMDB_API_KEY from '../../config/keys';
 import CarouselItemList from './CarouselItemList';
+import { Triple } from 'react-preloading-component';
 // import { Link } from 'react-router-dom';
 
 class CarouselPage extends Component {
@@ -121,61 +122,7 @@ componentDidMount(){
     
     // return {render};
     return(
-      <Container fluid>
-        {/* <h4 className="mt-5 mb-2">Basic example</h4> */}
-        <Carousel
-          activeItem={1}
-          length={4}
-          showControls={true}
-          showIndicators={true}
-          className="z-depth-1">
-          <CarouselInner>
-            <CarouselItem itemId="1">
-              <View>
-                <img className="d-block w-100" src='https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg' alt="First slide" />
-                <Mask overlay="black-light"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">View More</h3>
-                {/* <Link to= "/chei"><Button color="elegant"> Primary</Button></Link> */}
-
-
-                <p>First text</p>
-              </CarouselCaption>
-            </CarouselItem>
-            <CarouselItem itemId="2">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg" alt="Second slide" />
-                <Mask overlay="black-strong"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Strong mask</h3>
-                <p>Second text</p>
-              </CarouselCaption>
-            </CarouselItem>
-            <CarouselItem itemId="3">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg" alt="Third slide" />
-                <Mask overlay="black-slight"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Slight mask</h3>
-                <p>Third text</p>
-              </CarouselCaption>
-            </CarouselItem>
-            <CarouselItem itemId="4">
-              <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20%28143%29.jpg" alt="Mattonit's item" />
-                <Mask overlay="black-light"></Mask>
-              </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Sopot Beach</h3>
-                <p>Taken june 21th by @mattonit</p>
-              </CarouselCaption>
-            </CarouselItem>
-          </CarouselInner>
-        </Carousel>
-      </Container>
+      <Triple />
     );
   }
 }
