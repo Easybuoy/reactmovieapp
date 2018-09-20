db96482937b9101812316d938f5ad6f01da66d6b
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-20 17:18:08 +0100

    [feature] added skills section  to about page

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -9,19 +9,116 @@ import aboutbimg from './aboutimg.JPG';
 import movielistings from './movielistings.PNG';
 import movieinfo from './movieinfo.PNG';
 import schoolinformationsystem from './school_information_system.PNG';
-
-
+import Button from '@material-ui/core/Button';
+// import NavigationIcon from '@material-ui/icons/Navigation';
+// import Icon from '@material-ui/core/Icon';
+import { Fa } from 'mdbreact';
 import Card from '../../card/Card';
+
+
+import './About.css';
+
+
+
 class About extends Component {
   render() {
-      console.log('enter')
+    
     return (
       <div>
         <img src={aboutbimg} alt="aboutimg" className="img-thumbnail" style={{width:'150px', height: '150px', borderRadius: '50%'}}></img>
           <h1> About</h1>
           <div className="container">
           
-            <p className="text-justify">Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras mattis iudicium purus sit amet fermentum.</p>
+            <p className="text-justify">Experienced Web Developer with a demonstrated history of working in the information technology and services industry. 
+            Skilled in NODE, REACT, MONGODB, PHP, JAVASCRIPT, HTML, Search Engine Optimization (SEO), and Computer Hardware. 
+            Strong engineering professional graduated from Aptech Computer Education.</p>
+
+            <div className="container">
+            <h1 className="text-center"> SKILLS</h1>
+            <Button variant="contained"  className="skillbutton success-color wh-color">
+            NODEJS
+            </Button>
+
+            <Button variant="contained" className="skillbutton primary-color wh-color">
+            REACTJS
+            </Button>
+
+            <Button variant="contained"  className="skillbutton success-color wh-color">
+            MONGODB
+            </Button>
+
+            <Button variant="contained"  className="skillbutton primary-color wh-color">
+            PHP
+            </Button>
+
+            <Button variant="contained"  className="skillbutton danger-color wh-color">
+            LARAVEL
+            </Button>
+
+            <Button variant="contained"  className="skillbutton primary-color wh-color">
+            YII
+            </Button>
+
+            <Button variant="contained" className="skillbutton danger-color wh-color">
+            CODEIGNITER
+            </Button>
+
+            <Button variant="contained" className="skillbutton danger-color wh-color">
+            HTML
+            </Button>
+
+            <Button variant="contained"  className="skillbutton primary-color wh-color">
+            CSS
+            </Button>
+
+            <Button variant="contained" className="skillbutton warning-color wh-color">
+            JAVASCRIPT
+            </Button>
+
+            <Button variant="contained" className="skillbutton purple wh-color">
+            REDUX
+            </Button>
+
+            <Button variant="contained" className="skillbutton danger-color wh-color">
+            ANGULAR
+            </Button>
+
+            <Button variant="contained"  className="skillbutton light-blue wh-color">
+            JAVA
+            </Button>
+
+            <Button variant="contained" color="primary" className="skillbutton wh-color">
+            API
+            </Button>
+
+            <Button variant="contained" className="skillbutton black wh-color">
+            <Fa icon="github" className="fav" />
+            GITHUB
+            </Button>
+
+            <Button variant="contained" className="skillbutton danger-color wh-color">
+            <Fa icon="gitlab" className="fav" />
+            GITLAB
+            </Button>
+
+
+            <Button variant="contained"  className="skillbutton light-blue wh-color">
+            <Fa icon="wordpress" className="fav" />
+            WORDPRESS
+            </Button>
+
+            <Button variant="contained"  className="skillbutton primary-color wh-color">
+            <Fa icon="joomla" className="fav" />
+            JOOMLA
+            </Button>
+
+            <Button variant="contained" className="skillbutton danger-color wh-color">
+            <Fa icon="amazon" className="fav" />
+            AWS
+            </Button>
+            </div>
+            <br></br>
+
 
             <div className="row">
 
