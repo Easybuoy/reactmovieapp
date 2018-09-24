bf7fd9b39f1fe7a3275f704515d2bc0c02282f78
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-24 21:29:39 +0100

    [feature] added searchbox component

--- src/components/pages/tvseries/Tvseries.js
+++ src/components/pages/tvseries/Tvseries.js
@@ -1,12 +1,109 @@
 import React, { Component } from 'react';
-
+import Input from '@material-ui/core/Input';
+import SearchBox from './SearchBox';
+import TMDB from '../../../config/keys';
+// import { Triple } from 'react-preloading-component';
+import Error from '../../error/Error';
+import Card from '../../card/Card';
+import PopularSeries from '../home/PopularSeries';
 
 class Tvseries extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        searchField: '',
+        searchResults: 0,
+        error: 0
+    };
+}
+  onSearchChange = (event) =>{
+    this.setState({searchField: event.target.value});
+// console.log(event.target.value);
+    fetch(`${TMDB.TMDB_PATH}search/tv?api_key=${TMDB.TMDB_API_KEY}&query=${event.target.value}&include_adult=true`, {
+      method: 'get',
+      headers: {    
+          'Content-Type': 'application/json'
+      }, 
+  }).then(res => res.json()
+  .then(data => {
+    this.setState({searchResults: data});
+  })  
+).catch(err =>{
+  this.setState({error: err});
+})
+}
+
+trimText = (text) => {
+  return text.substring(0, 50) + '...';
+}
+
   render() {
-      console.log('enter')
+    console.log(this.state)
+
+
+    if(this.state.searchResults !== 0){
+      let searchresults =  this.state.searchResults.results;
+      // let casts = credits.cast;
+      if(searchresults){
+
+          return(
+              <div className="container">    
+              <SearchBox searchChange = {this.onSearchChange}/>
+
+              <h1 className="text-center">RESULTS</h1>
+            <hr></hr>
+          
+          <div className="row">                
+         {
+             
+                  searchresults.map((searchresult, i) => {
+                   return (
+                      <div className="col-lg-4 col-md-4 col-sm-4">
+                      <Card imageURL={`${TMDB.IMG_PATH + 'w500/' + searchresult.backdrop_path}`} cardTitle={searchresult.original_name}
+                      cardText = {this.trimText(searchresult.overview)} 
+                      buttonURL={`/series/${searchresult.id}`} buttonText="View More" ReloadLink="true"
+                      />
+                      <br></br> 
+                  </div>
+
+                   ); 
+
+                  })
+
+          }   
+
+
+            </div>
+           </div>
+        );
+
+
+      }
+
+    }else if(this.state.error !== 0){
+     
+            return (
+            <div>
+                <Error />
+            </div>
+            );
+
+    }else{
+        // return (
+        //     <div>
+        //       <Triple />
+        //     </div>
+        //     )
+  }
+
+
     return (
       <div>
-          <h1> Tvseries</h1>
+          {/* <h1> Movies</h1> */}
+          <div className="container">
+          <SearchBox searchChange = {this.onSearchChange}/>
+          <PopularSeries />
+          </div>
       </div>
     );
   }
