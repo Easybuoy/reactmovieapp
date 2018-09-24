97744cbdf7261068267fe390323ad958d6019b89
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-09-24 15:20:39 +0100

    [feature] added thanks component

---
+++ src/components/thanks/Thanks.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './Thanks.css';
+
+class Thanks extends Component {
+
+  render(){
+
+ 
+    return(
+      <div className="container">
+      
+      <div style={{marginTop: '10%', marginBottom: '10%'}}>
+      <h1>Thanks For Reaching Out</h1>
+      <h3> I Will Be In Touch Shortly</h3>
+      <a href='/'><input className="btn btn-outline-brown" type="button" value="Back To Home Page " /></a>
+      </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default Thanks;
+
